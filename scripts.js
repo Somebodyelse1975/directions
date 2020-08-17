fd38b5395a647cee612d9f820d7a91291ef122fa
Somebodyelse1975 <69575778+Somebodyelse1975@users.noreply.github.com>
2020-08-17 23:04:48 +0200

    uploaded

---
+++ scripts.js
@@ -0,0 +1,21 @@
+let color = 'purple';
+console.log('color');
+let number = 10;
+console.log('number');
+let word = 'cool';
+
+if (color === 'purple') {
+    $('.red').css('background-color', 'purple');
+};
+
+if (number > 100) {
+    $('.yellow').text('whoah, that is a big number.')
+} else {
+    $('yellow').text('just a regular number, please.')
+};
+
+if (word === 'cool') {
+    $('.green').text('Power of DOM');
+} else if (word !== 'cool') {
+    $('#blue').text('Power of DOM');
+}
